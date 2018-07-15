0b3bf0e423d92d6a3e60bea886026a2b9714706a
Ido Weiss <idow09@gmail.com>
2018-07-15 22:26:32 +0000

    got rid of the old http server stuff in myserver. it's available in its own dir if necessary. myserver is now the simplest lkm.

--- myserver/myserver.c
+++ myserver/myserver.c
@@ -1,35 +1,15 @@
-/*  
- *   *  hello-1.c - The simplest kernel module.
- *    */
 #include <linux/module.h>	/* Needed by all modules */
 #include <linux/kernel.h>	/* Needed for KERN_INFO */
 #include "httpd.h"
 
 int init_module(void)
 {
-		printk(KERN_INFO "Hello world 1.\n");
-
-			/* 
-			 * 	 * A non 0 return means init_module failed; module can't be loaded. 
-			 * 	 	 */
-		serve_forever("12913");
-		return 0;
+	printk(KERN_INFO "Hello world 1.\n");
+	return 0;
 }
 
 void cleanup_module(void)
 {
-		printk(KERN_INFO "Goodbye world 1.\n");
+	printk(KERN_INFO "Goodbye world 1.\n");
 }
 
-void route()
-{
-	ROUTE_START()
-
-	ROUTE_GET("/")
-	{
-		printf("HTTP/1.1 200 OK\r\n\r\n");
-		printf("Hello! You are using %s", request_header("User-Agent"));
-	}
-
-	ROUTE_END()
-}
